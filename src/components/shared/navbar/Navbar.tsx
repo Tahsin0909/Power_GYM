c236854795b996eafce4fcadef885050a824bbcf
Tahsin Zaman <tajisan789@gmail.com>
2024-10-04 02:54:56 +0600

    Added dependicies and working on navbar

---
+++ src/components/shared/navbar/Navbar.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
+import { useState, useRef, useEffect } from "react";
+
+const Navbar = () => {
+    // State to control the display of the menu and active navlink
+    const [activeNav, setActiveNav] = useState('Home');  // State for active nav
+
+    // State to toggle the visibility of the menu
+    const [toggle, setToggle] = useState<boolean>(false);
+
+    // Ref to track the navbar menu element for outside click detection
+    const ref = useRef<HTMLDivElement>(null);
+
+    // Toggle the menu open or closed when the menu icon is clicked
+    const handleToggle = (event: React.MouseEvent) => {
+        event.stopPropagation(); // Prevents triggering handleClickOutside when clicking inside
+        setToggle((prevToggle) => !prevToggle); // Toggle the state
+    };
+
+    // Handles clicks outside the menu; closes the menu if clicked outside
+    const handleClickOutside = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+            setToggle(false); // Close the menu
+        }
+    };
+
+    // Effect hook to listen for clicks outside the navbar and clean up
+    useEffect(() => {
+        if (window.innerWidth > 425) {
+            setToggle(false);
+        }
+        document.addEventListener("mousedown", handleClickOutside); // Listen for outside clicks
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside); // Clean up listener on unmount
+        };
+    }, []);
+
+
+    // Handle active navigation link
+    const handleActiveNav = (link: string) => {
+        setActiveNav(link);
+    };
+
+    // Array for navigation links
+    const navLinks = [
+        { name: 'Home', href: '#' },
+        { name: 'Team', href: '#' },
+        { name: 'Feature', href: '#' },
+        { name: 'Blog', href: '#' },
+        { name: 'About', href: '#' },
+        { name: 'Contact', href: '#' }
+    ];
+
+    return (
+        <header className='flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50'>
+            <div className='flex flex-wrap items-center justify-between gap-5 w-full'>
+                <a href="#">
+                    <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+                </a>
+
+                <div
+                    id="collapseMenu"
+                    className={`lg:!block ${toggle ? 'block w-fit' : 'hidden'} max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
+                >
+                    <div ref={ref}>
+                        <ul 
+                            className='lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
+                            <li className='mb-6 hidden max-lg:block'>
+                                <a href="#">
+                                    <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+                                </a>
+                            </li>
+                            {/* Map through navigation links */}
+                            {navLinks.map((link, index) => (
+                                <li key={index} className='max-lg:border-b border-gray-300 max-lg:py-3 px-3'>
+                                    <a
+                                        href={link.href}
+                                        className={`block font-semibold text-[15px] ${activeNav === link.name ? 'text-[#007bff]' : 'text-gray-500'} hover:text-[#007bff]`}
+                                        onClick={() => handleActiveNav(link.name)}
+                                    >
+                                        {link.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className='flex max-lg:ml-auto space-x-3'>
+                    <button
+                        className='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]'>
+                        Login
+                    </button>
+                    <button
+                        className='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]'>
+                        Sign up
+                    </button>
+
+                    <button id="toggleOpen" onClick={handleToggle} className='lg:hidden'>
+                        <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clipRule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,64 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#1E99F5',
+        secondary: '#FAFAFA',
+        activeNav: "#F8F8F8",
+        footer_link: "#81859F",
+        error: "#ef4444",
+        discount: "#B92E2E"
       },
+      margin: {
+        m_small: '4px',
+        m_base: '12px',
+        m_primary: '16px',
+        m_secondary: '24px',
+        m_lg: '32px',
+        m_xl: '60px',
+      },
+      gap: {
+
+        gap_base: "16px",
+        gap_primary: "20px"
+      },
+      fontSize: {
+        text_small: '13px',
+        text_base: '18px',
+        text_primary: '30px',
+        text_secondary: '36px',
+        text_lg: '16px',
+        text_xl: '20px'
+      },
+      padding: {
+        input_padding: "8px",
+        button_padding: "12px",
+        padding_base: "32px",
+        padding_small: "8px",
+        padding_medium: "12px",
+        padding_md: "20px",
+        padding_lg: "60px",
+        padding_xl: "120px"
+      },
+      borderRadius: {
+        rounded_primary: '8px',
+        rounded_secondary: '16px',
+        rounded_4px: '4px',
+
+      },
+      height: {
+        h_base: "20px",
+        h_medium: "48px",
+        h_110vh: "110vh"
+
+      },
+      width: {
+        w_base: "20px",
+        w_medium: "48px",
+        w_drawer: "300px",
+        w_logo: "144px",
+        w_445px: "445px",
+      }
+
     },
   },
   plugins: [],
