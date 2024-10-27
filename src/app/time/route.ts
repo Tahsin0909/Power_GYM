9d6b244a7806af5b394c747aad29747311b1f64b
Tahsin Zaman <tajisan789@gmail.com>
2024-10-27 22:46:45 +0600

    fderttyf

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -4,3 +4,10 @@ export const GET = async () =>{
         currentTime : new Date().toLocaleDateString()
     })
 }
+
+// just for git push
+export const POST = async () =>{
+    return Response.json({
+        currentTime : new Date().toLocaleDateString()
+    })
+}
