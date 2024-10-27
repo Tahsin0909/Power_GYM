2f9fa4399531cac30921067c603638c15e70b6bf
Tahsin Zaman <tajisan789@gmail.com>
2024-10-27 22:45:32 +0600

    fdef

--- src/components/ratinngs/Ratings.tsx
+++ src/components/ratinngs/Ratings.tsx
@@ -14,19 +14,3 @@ const Ratings = ({ totalRatings, ratted }: { totalRatings: number, ratted: numbe
 };
 
 export default Ratings;
-
-const Ratings = ({ totalRatings, ratted }: { totalRatings: number, ratted: number }) => {
-
-
-    return (
-        <div className="text-white flex gap-1">
-            {
-                Array.from({ length: totalRatings }, (_, idx) => (
-                    <svg key={idx} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`lucide lucide-sparkles ${idx < ratted ? "stroke-blue-500 fill-blue-500" : ""}`}><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/></svg>
-                ))
-            }
-        </div>
-    );
-};
-
-export default Ratings;
