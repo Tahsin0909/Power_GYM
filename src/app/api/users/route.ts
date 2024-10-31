9005bb2b3999bac4f540e6c07fde40cd0a4b4a48
Tahsin Zaman <tajisan789@gmail.com>
2024-10-31 23:49:11 +0600

    AAds

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,6 @@
 
 
-export const GET = async () => {
-    return Response.json({
-        message: "hello Users"
-    })
-}
+
 
 
 interface User {
@@ -24,6 +20,20 @@ interface NewUserRequest extends Request {
 const users: User[] = [];
 
 export const POST = async (request: NewUserRequest): Promise<Response> => {
+    const newUsers = await request.json(); // Await the JSON parsing
+
+    users.push({
+        id: users.length + 1,  // Incrementing id based on the existing users' length
+        name: newUsers.name,
+        password: newUsers.password
+    });
+
+    return Response.json({
+        message: "user added",
+        users
+    });
+};
+export const GET = async (request: NewUserRequest): Promise<Response> => {
     const newUsers = await request.json(); // Await the JSON parsing
 
     users.push({
