0861d1fb5a6267eabcad00e33accb663178d60c6
Tahsin Zaman <tajisan789@gmail.com>
2024-11-05 23:15:16 +0600

    ssd

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -76,3 +76,16 @@ export const DELETE = async (request: NewUserRequest, { params }: { params: { id
         users
     });
 };
+export const put = async (request: NewUserRequest, { params }: { params: { id: string } }): Promise<Response> => {
+    const id = params.id
+    const indexUser = users.findIndex((c) => c.id === parseInt(id))
+    users[indexUser] = {
+        id: users[indexUser].id,
+        name: users[indexUser].name,
+        password: "changed pass"
+    }
+    return Response.json({
+        message: "user added",
+        users
+    });
+};
